f692cec1dbce9f9f58ff97029a2d0a830f8abc7c
Thomas Hourlier <thomas.hourlier@intercom.io>
2018-05-15 22:58:46 +0100

    Add comment to be more explicit

--- app/models/father_mother_relation.rb
+++ app/models/father_mother_relation.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class FatherMotherRelation < Relation
+  # first_mount will be the father
+  # second_mount will be the mother
 end
--- app/models/parent_child_relation.rb
+++ app/models/parent_child_relation.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ParentChildRelation < Relation
+  # first_mount will be the parent
+  # second_mount will be the child
 end
