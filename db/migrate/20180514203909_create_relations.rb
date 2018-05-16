d389cc6b857ec3e548c3a3dcacd96e3fa6073972
Thomas Hourlier <thomas.hourlier@intercom.io>
2018-05-16 17:50:16 +0100

    optimize db indexes

--- db/migrate/20180514203909_create_relations.rb
+++ db/migrate/20180514203909_create_relations.rb
@@ -11,6 +11,7 @@ class CreateRelations < ActiveRecord::Migration[5.2]
 
     add_foreign_key :relations, :mounts, column: :first_mount_id
     add_foreign_key :relations, :mounts, column: :second_mount_id
-    add_index :relations, %i[type first_mount_id second_mount_id], unique: true
+    add_index :relations, %i[type first_mount_id], unique: true
+    add_index :relations, %i[type second_mount_id], unique: true
   end
 end
