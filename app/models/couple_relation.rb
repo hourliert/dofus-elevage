d41d1e7b0d0abaee5d880ee102e039962f0806a7
Thomas Hourlier <thomas.hourlier@intercom.io>
2018-05-17 20:00:07 +0100

    Add model validation

--- app/models/couple_relation.rb
+++ app/models/couple_relation.rb
@@ -3,4 +3,13 @@
 class CoupleRelation < Relation
   # first_mount will be the father
   # second_mount will be the mother
+
+  validate :first_mount, :mounts_must_have_different_sexe
+  validate :second_mount, :mounts_must_have_different_sexe
+
+  private def mounts_must_have_different_sexe
+    if first_mount.sexe == second_mount.sexe
+      errors.add(:mount_sexes, "Couple members must have different sexes")
+    end
+  end
 end
