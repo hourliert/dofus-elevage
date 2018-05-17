dfcf1ab6bb5e5115ab098406ee84940e3754b9be
Thomas Hourlier <hourliert@gmail.com>
2018-05-17 20:00:48 +0100

    Add model validation (#2)

--- app/models/family_relation.rb
+++ app/models/family_relation.rb
@@ -3,4 +3,12 @@
 class FamilyRelation < Relation
   # first_mount will be the parent
   # second_mount will be the child
+  validate :first_mount, :mounts_must_have_same_family
+  validate :second_mount, :mounts_must_have_same_family
+
+  private def mounts_must_have_same_family
+    if first_mount.generation.family != second_mount.generation.family
+      errors.add(:mount_families, "Family members must share the same race")
+    end
+  end
 end
--- app/models/relation.rb
+++ app/models/relation.rb
@@ -3,4 +3,7 @@
 class Relation < ApplicationRecord
   belongs_to :first_mount, class_name: "Mount"
   belongs_to :second_mount, class_name: "Mount"
+
+  validates :first_mount, presence: true
+  validates :second_mount, presence: true
 end
